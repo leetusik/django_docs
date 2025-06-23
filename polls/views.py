6091e501d3a5fe332e762f53113138a655a0844a
leetusik <leetusik@gmail.com>
2025-06-24 00:49:31 +0900

    generic views

--- polls/views.py
+++ polls/views.py
@@ -2,35 +2,28 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-# Create your views here.
-def index(request):
-    questions = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "questions": questions,
-    }
-    return render(
-        request,
-        "polls/index.html",
-        context,
-    )
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(
-        request,
-        "polls/detail.html",
-        {"question": question},
-    )
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -39,13 +32,14 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        }
         return render(
             request,
             "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
+            context,
         )
     else:
         selected_choice.votes = F("votes") + 1
